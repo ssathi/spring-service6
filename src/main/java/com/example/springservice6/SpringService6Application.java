d34ba872aa832148a52c51b5d0f74e6b3b0d710d
sathi2 <sathi@wired2perform.com>
2019-02-17 13:06:22 +0530

    service repo 6 - ff00f84b719e7a73f1c3d37635ca93459371beac

---
+++ src/main/java/com/example/springservice6/SpringService6Application.java
@@ -0,0 +1,13 @@
+package com.example.springservice6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringService6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringService6Application.class, args);
+	}
+
+}
